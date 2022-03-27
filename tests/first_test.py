ef695c75ee42aeb53eed349f53f073f03d9071e8
Олег К <megaros22@gmail.com>
2022-03-27 19:13:17 +0300

    Отредактированы имена функций

--- tests/first_test.py
+++ tests/first_test.py
@@ -5,14 +5,14 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_multiply_calculate_correctly(self):
+    def test_multiply_calculate(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_division_correctly(self):
+    def test_division(self):
         assert self.calc.division(self, 6, 3) == 2.0
 
-    def test_subtraction_correctly(self):
+    def test_subtraction(self):
         assert self.calc.subtraction(self, 6, 3) == 3
 
-    def test_adding_correctly(self):
+    def test_adding(self):
         assert self.calc.adding(self, 6, 3) == 9
