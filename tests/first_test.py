8abb014fe52c8c9e47dc7aee5e82641a23239fa5
Олег К <megaros22@gmail.com>
2022-07-29 20:29:50 +0300

    Добавил Print

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,4 +1,4 @@
-# import pytest
+import pytest
 from app.calculator import Calculator
 
 class TestCalc:
@@ -6,13 +6,17 @@ class TestCalc:
         self.calc = Calculator
 
     def test_multiply_calculate(self):
+        print ('\nПроверка умножения')
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_division(self):
+        print('\nПроверка деления')
         assert self.calc.division(self, 6, 3) == 2.0
 
     def test_subtraction(self):
+        print('\nПроверка вычетания')
         assert self.calc.subtraction(self, 6, 3) == 3
 
     def test_adding(self):
+        print('\nПроверка сложения')
         assert self.calc.adding(self, 6, 3) == 9
